d3bc68334e9b2563bdb132c34bb1bc8537b5b4bf
Derek Huit <derekhuit@gmail.com>
2026-04-08 23:30:40 -0800

    feat: add Huit.AI ASE widget via _document [AXIS]

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          {/* Huit.AI Autonomous Sales Engine */}
+          <script src="https://ase.huit.ai/widget.js" data-product="AXIS" defer></script>
+        </body>
+      </Html>
+    );
+  }
+}
